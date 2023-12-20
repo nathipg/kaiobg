71dea619f00235bd52e79e1f615a8e42e16e49c2
kaiobg <kaiobg@hotmail.com>
2023-12-20 16:06:26 -0300

    trein

--- docs/treinomodelo.js
+++ docs/treinomodelo.js
@@ -19,14 +19,10 @@ var v = document.getElementsByClassName('myVideo');
     document.getElementById('toVideo2').addEventListener('click', function() { classToggle(1),closeOtherVideo(1)});
     document.getElementById('toVideo3').addEventListener('click', function() { classToggle(2),closeOtherVideo(2)});
     document.getElementById('toVideo4').addEventListener('click', function() { classToggle(3),closeOtherVideo(3)});
-    
-    window.onload = function () {
-           var imc= 8
-        $("#includedContent").load(imc);
-      
-    }
-        var imc = paciente.querySelector(".imc");
+    document.getElementById('toVideo5').addEventListener('click', function() { classToggle(4),closeOtherVideo(4)});
+    document.getElementById('toVideo6').addEventListener('click', function() { classToggle(5),closeOtherVideo(5)});
+    document.getElementById('toVideo7').addEventListener('click', function() { classToggle(6),closeOtherVideo(6)});
+    document.getElementById('toVideo8').addEventListener('click', function() { classToggle(7),closeOtherVideo(7)});
 
-        var himc = peso / (altura * altura);
-        
-        imc.textContent = himc;
+    
+    var idStr = element.id;
